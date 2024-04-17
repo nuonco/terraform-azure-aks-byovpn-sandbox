7e7eb66215fcec057d5a917a834b22def3f21d2e
Jon Morehouse <morehousej09@gmail.com>
2024-04-17 08:25:26 -0700

    chore: remove public dns

--- dns.tf
+++ dns.tf
@@ -1,8 +1,3 @@
-#resource "azurerm_dns_zone" "public" {
-  #name                = var.public_root_domain
-  #resource_group_name = data.azurerm_resource_group.rg.name
-#}
-
 resource "azurerm_private_dns_zone" "internal" {
   name                = var.internal_root_domain
   resource_group_name = data.azurerm_resource_group.rg.name

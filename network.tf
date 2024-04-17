dc0ca9986aa4cdab6c06cdc2f9949afcb325b942
Jon Morehouse <MorehouseJ09@gmail.com>
2024-04-16 23:17:28 -0600

    chore: remove appgw from byo-vpn sandbox (#11)

--- network.tf
+++ network.tf
@@ -1,15 +1,4 @@
-locals {
-  // we create a network with two address spaces - one for node pool subnets and one for services, gateways etc.
-  address_spaces = ["10.0.0.0/16", "10.2.0.0/16"]
-  // node pool subnets
-  subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names = ["a", "b", "c"]
-
-  // app and services
-  appgw_cidr     = "10.2.0.0/24"
-  service_cidr   = "10.2.1.0/24"
-  dns_service_ip = "10.2.1.10"
-}
+locals {}
 
 data "azurerm_virtual_network" "network" {
   name                = var.network_name
